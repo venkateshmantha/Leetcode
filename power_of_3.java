dfe6f8794b32163507d7dc7a40d2a7726a3b9c51
venkateshmantha <manthavenkatesh@gmail.com>
2017-07-07 23:25:30 -0400

    Day13

---
+++ power_of_3.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n>1){
+            while(n%3==0){
+                n = n/3;
+            }
+        }
+        return n==1;
+    }
+}
