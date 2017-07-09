762d7c21c27df7614df8181eb78f1380883831cc
venkateshmantha <manthavenkatesh@gmail.com>
2017-07-09 00:51:20 -0400

    day14

--- contigous_array.java
+++ contigous_array.java
@@ -1,23 +1,19 @@
 public class Solution {
     public int findMaxLength(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) nums[i] = -1;
+        int cnt=0;
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        map.put(0,-1);
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)
+                cnt--;
+            else
+                cnt++;
+        if(map.containsKey(cnt)){
+            max = Math.max(max,i-map.get(cnt));
         }
-        
-        Map<Integer, Integer> sumToIndex = new HashMap<>();
-        sumToIndex.put(0, -1);
-        int sum = 0, max = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (sumToIndex.containsKey(sum)) {
-                max = Math.max(max, i - sumToIndex.get(sum));
-            }
-            else {
-                sumToIndex.put(sum, i);
-            }
+        else map.put(cnt,i);
         }
-        
         return max;
     }
 }
