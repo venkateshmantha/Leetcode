f00ac372568e455398a04169d321d7afca655579
venkateshmantha <manthavenkatesh@gmail.com>
2017-07-11 23:34:04 -0400

    day 17

---
+++ valid_perfect_square.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high)/2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
