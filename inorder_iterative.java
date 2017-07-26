a9a7613c919332aceac83a013bb63d532566ebfc
venkateshmantha <manthavenkatesh@gmail.com>
2017-07-25 22:50:57 -0400

    day31

---
+++ inorder_iterative.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<Integer>();
+        if (root == null) return res;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) { 
+            while (cur != null) {// Travel to each node's left child, till reach the left leaf
+                stack.push(cur);
+                cur = cur.left;				
+            }		 
+            cur = stack.pop(); // Backtrack to higher level node A
+            res.add(cur.val);  // Add the node to the result list
+            cur = cur.right;   // Switch to A'right branch
+        }
+        return res;
+    }
+}
