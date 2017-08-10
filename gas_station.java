e88edee1872cba155ea21cb35ce34bc923254f62
venkateshmantha <manthavenkatesh@gmail.com>
2017-08-10 17:28:24 -0400

    day48

---
+++ gas_station.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int size=gas.length;
+        int sum=0;
+        int res=0;
+        int total=0;
+        for(int i=0; i<size; ++i){
+            sum+=gas[i]-cost[i];
+            if(sum<0){
+                total+=sum;
+                sum=0;
+                res=i+1;
+            }
+        }
+        total+=sum;
+        return total<0?-1:res;
+    }
+}
