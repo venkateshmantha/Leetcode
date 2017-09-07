6eda363a97b50c9c7c6d43637034840217e331f3
Venkatesh Mantha <venkateshmantha@Venkateshs-MacBook-Pro.local>
2017-09-06 21:11:40 -0400

    day72

---
+++ zero_matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        boolean[] row = new boolean[r];
+        boolean[] col = new boolean[c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+        }
+        
+        // Nullify rows
+		for (int i = 0; i < row.length; i++) {
+			if (row[i]) {
+				nullifyRow(matrix, i);
+			}
+		}
+		
+		// Nullify columns
+		for (int j = 0; j < col.length; j++) {
+			if (col[j]) {
+				nullifyColumn(matrix, j);
+			}
+		}
+    }
+    
+    public static void nullifyRow(int[][] matrix, int row) {
+		for (int j = 0; j < matrix[0].length; j++) {
+			matrix[row][j] = 0;
+		}		
+	}
+
+	public static void nullifyColumn(int[][] matrix, int col) {
+		for (int i = 0; i < matrix.length; i++) {
+			matrix[i][col] = 0;
+		}		
+	}			
+
+}
+ 
