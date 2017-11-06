d663ed0f046b2437651b0bb56e0cd8ee94c0db90
venkateshmantha <manthavenkatesh@gmail.com>
2017-11-05 20:07:24 -0500

    trees

---
+++ populating_next_right_pointers.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode level_start=root;
+        while(level_start!=null){
+            TreeLinkNode cur=level_start;
+            while(cur!=null){
+                if(cur.left!=null) cur.left.next=cur.right;
+                if(cur.right!=null && cur.next!=null) cur.right.next=cur.next.left;
+                
+                cur=cur.next;
+            }
+            level_start=level_start.left;
+        }
+        
+    }
+}
