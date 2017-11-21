2b6526237ad4d9279f120615cd2c2dc9dd582586
venkateshmantha <manthavenkatesh@gmail.com>
2017-11-21 11:04:06 -0500

    calculator 1

--- calculator.java
+++ calculator.java
@@ -1,29 +1,33 @@
 class Solution {
     public int calculate(String s) {
-        int len = s.length(), sign = 1, result = 0;
-	Stack<Integer> stack = new Stack<Integer>();
-	for (int i = 0; i < len; i++) {
-		if (Character.isDigit(s.charAt(i))) {
-			int sum = s.charAt(i) - '0';
-			while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
-				sum = sum * 10 + s.charAt(i + 1) - '0';
-				i++;
-			}
-			result += sum * sign;
-		} else if (s.charAt(i) == '+')
-			sign = 1;
-		else if (s.charAt(i) == '-')
-			sign = -1;
-		else if (s.charAt(i) == '(') {
-			stack.push(result);
-			stack.push(sign);
-			result = 0;
-			sign = 1;
-		} else if (s.charAt(i) == ')') {
-			result = result * stack.pop() + stack.pop();
-		}
-
-	}
-	return result;
+        Stack<Integer> stack = new Stack();
+        int sign = 1;
+        int res = 0;
+        int num = 0;
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            if(Character.isDigit(s.charAt(i))){
+                num = s.charAt(i) - '0';
+                while(i+1<len && Character.isDigit(s.charAt(i+1))){
+                		num = num*10 + s.charAt(i+1) - '0';
+                		i++;
+                }
+                res += num*sign;
+            }
+            else if(s.charAt(i) == '+')
+                sign = 1;
+            else if(s.charAt(i) == '-')
+                sign = -1;
+            else if(s.charAt(i) == '('){
+                stack.push(sign);
+                stack.push(res);
+                res = 0;
+                sign = 1;
+            }
+            else if(s.charAt(i) == ')'){
+                res = stack.pop() + res*stack.pop();
+            }
+        }
+        return res;
     }
 }
